5f7bc4fe140c4a43139f26c4997f729fc75e914a
Zamiur Rahman Ratul <Ratul@Ratuls-MacBook-Pro.local>
2016-08-10 23:28:58 +0600

    first_commit

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+	private Camera mainCamera;
+
+	void Awake(){
+		mainCamera = Camera.main;
+	}
+
+	public void ChangeColor () {
+		mainCamera.backgroundColor = Random.ColorHSV ();
+	}
+}
